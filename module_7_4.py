4ad3fb52e6a673111a7e15ac438283c304b021d9
Лена <trylya@mail.ru>
2024-11-14 21:03:26 +0300

    Форматирование строк

--- module_7_4.py
+++ module_7_4.py
@@ -4,8 +4,8 @@ team2_num = 6
 score_1 = 40
 score_2 = 42
 
-team1_time = 1552.512  # время за которое команда 1 решила задачи
-team2_time = 2153.31451  # время за которое команда 2 решила задачи
+team1_time = 1552.512
+team2_time = 2153.31451
 
 print("В команде Мастера кода участников: %s ! " % team1_num)
 print("Итого сегодня в командах участников: %s и %s !" % (team1_num, team2_num))
@@ -24,7 +24,7 @@ else:
     challenge_result = 'Ничья!'
 print(f'Результат битвы: {challenge_result}')
 
-tasks_total = score_1 + score_2  # количество задач
-time_avg = (team1_time + team2_time) / tasks_total  # среднее время решения
+tasks_total = score_1 + score_2
+time_avg = (team1_time + team2_time) / tasks_total
 print(f'Сегодня было решено {tasks_total} задач,',
       f'в среднем по {time_avg:.1f} секунды на задачу!')
